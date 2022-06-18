93f54a739d6a74e33fdf41494192de6734805462
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-06-18 09:55:22 +0530

    Create side bar components

--- responsive-sidenavbar/src/index.js
+++ responsive-sidenavbar/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
